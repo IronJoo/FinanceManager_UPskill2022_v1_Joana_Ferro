616db0e65978747db5c254998fd2f0f2d927fd26
IronJoo <joanaferro92@gmail.com>
2022-06-20 02:09:32 +0100

    Modified FileAccountFormat

--- src/pt/upskill/projeto2/financemanager/accounts/formats/FileAccountFormat.java
+++ src/pt/upskill/projeto2/financemanager/accounts/formats/FileAccountFormat.java
@@ -3,6 +3,6 @@ package pt.upskill.projeto2.financemanager.accounts.formats;
 public class FileAccountFormat implements Format {
     @Override
     public String format(Object objectToFormat) {
-        return null;
+        return "";
     }
 }
